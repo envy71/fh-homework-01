c9cd5d819125741b8846dc24a0e0760176874cc6
Nickolas Vu <nickvu71@gmail.com>
2019-07-31 21:10:28 +0000

    added gem pg

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170414190158) do
+ActiveRecord::Schema.define(version: 20190724215140) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "authors", force: :cascade do |t|
     t.string   "first_name"
@@ -18,6 +21,14 @@ ActiveRecord::Schema.define(version: 20170414190158) do
     t.integer  "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "places", id: :bigserial, force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "address"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
